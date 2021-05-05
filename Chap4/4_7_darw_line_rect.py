93049102e0ee36778d60f8b34f411b4a372a4faa
junseongwoo <wnstjd4852@gmail.com>
2021-05-05 12:50:04 +0900

    0505 커밋

--- Chap4/4_7_darw_line_rect.py
+++ Chap4/4_7_darw_line_rect.py
@@ -1,4 +1,4 @@
-#import numpy as np
+import numpy as np
 import cv2
 
 blue, green, red = (255, 0, 0), (0, 255, 0), (0, 0, 255)
@@ -11,8 +11,17 @@ pt3, pt4 = (400, 150), (500, 50)
 roi = (50, 200, 200, 100)
 
 ## 직선 그리기 
-cv2.line
-getattr
+cv2.line(image, pt1, pt2, red)
+cv2.line(image, pt3, pt4, green, 3, cv2.LINE_AA)    ## cv2.LINE_AA : 계단 현상을 감소 시킨 선
+
+## 사격형 그리기 
+cv2.rectangle(image, pt1, pt2, blue, 3, cv2.LINE_4) ## cv2.LINE_4 : 4-방향 연결선
+cv2.rectangle(image, roi, red, 3, cv2.LINE_8 )      ## cv2.LINE_8 : 8-방향 연결선
+cv2.rectangle(image, (400, 200, 100, 100), green, cv2.FILLED)
+
+cv2.imshow("Line & Rectangle", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
