142513273f07b8c7a1aeccb6144e5c68b3cb4d8b
junseongwoo <wnstjd4852@gmail.com>
2021-07-08 16:25:17 +0900

    inpaint

--- inpainting.py
+++ inpainting.py
@@ -20,9 +20,9 @@ noise = cv2.imread('./images/img2.png')
 noise = cv2.cvtColor(noise, cv2.COLOR_BGR2GRAY)
 noise = cv2.resize(noise, (480, 480))
 
-dst = cv2.inpaint(noise_img, noise, 3, cv2.INPAINT_TELEA)
+dst = cv2.inpaint(noise_img, noise, 3, cv2.INPAINT_NS)
 
-cv2.imshow('python', noise_img)
+cv2.imshow('normal', noise_img)
 cv2.imshow('noise', noise)
 cv2.imshow('inpaint', dst)
 cv2.waitKey(0)
