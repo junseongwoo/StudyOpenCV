3f13a8c4e30127fa1ad963894d525f766680ac86
junseongwoo <wnstjd4852@gmail.com>
2021-07-18 14:59:59 +0900

    Watershed Commit

--- Watershed.py
+++ Watershed.py
@@ -9,6 +9,21 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 _, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
+# noise removal 
+kernel = np.ones((3,3), np.uint8)
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 2)
+
+# sure background area
+sure_bg = cv2.dilate(opening, kernel, iterations = 3)
+
+# Finding sure foreground area
+dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+_, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+# Finding unknown region 
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg, sure_fg)
+
 cv2.imshow("thresh", thresh)
 
 cv2.waitKey(0)
