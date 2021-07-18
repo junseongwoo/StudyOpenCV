3adee5adc7aac3fddb8e22f0579461c9e942d732
junseongwoo <wnstjd4852@gmail.com>
2021-07-18 14:56:22 +0900

    Watershed Commit

--- Watershed.py
+++ Watershed.py
@@ -5,19 +5,11 @@ import cv2
 import matplotlib.pyplot as plt 
 
 img = cv2.imread('./images/water_coins.jpg')
-img1 = cv2.imread('./images/water_coins.jpg', 0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-_, thresh = cv2.threshold(img1, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+_, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("thresh", thresh)
 
-
-plt.subplot(2,2,1)
-plt.hist(img1.ravel(), 256)
-plt.title('what')
-plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2)
-plt.hist(gray.ravel(), 256)
-plt.title('good')
-plt.xticks([]), plt.yticks([])
-plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
